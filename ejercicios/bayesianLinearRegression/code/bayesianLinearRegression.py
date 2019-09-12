4093eb90047d6aab2f17e3cfc54885cc04cbbfa8
Gustavo Landfried <gustavolandfried@gmail.com>
2019-09-12 16:32:25 -0300

    Bayesian Linear Regression

---
+++ ejercicios/bayesianLinearRegression/code/bayesianLinearRegression.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def posterior(Phi, t, alpha, beta, return_inverse=False):
+    """Computes mean and covariance matrix of the posterior distribution."""
+    S_N_inv = alpha * np.eye(Phi.shape[1]) + beta * Phi.T.dot(Phi)
+    S_N = np.linalg.inv(S_N_inv)
+    m_N = beta * S_N.dot(Phi.T).dot(t)
+
+    if return_inverse:
+        return m_N, S_N, S_N_inv
+    else:
+        return m_N, S_N
+
+
+def posterior_predictive(Phi_test, m_N, S_N, beta):
+    """Computes mean and variances of the posterior predictive distribution."""
+    y = Phi_test.dot(m_N)
+    # Only compute variances (diagonal elements of covariance matrix)
+    y_var = 1 / beta + np.sum(Phi_test.dot(S_N) * Phi_test, axis=1)
+    
+    return y, y_var
+
