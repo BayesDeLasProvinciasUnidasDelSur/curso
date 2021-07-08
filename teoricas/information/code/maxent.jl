b4df3287140a00cfe62adce13ad8c1fd42379fd8
Gustavo Landfried <gustavolandfried@gmail.com>
2021-07-08 12:52:11 -0300

    Código MaxEnt, information: el posterior como la distribución que más se parece al prior (KL-divergence) luego de incorporar la evidencia

---
+++ teoricas/information/code/maxent.jl
@@ -0,0 +1,27 @@
+using Plots
+
+global _prior_ = [ [0 0 0] ; [1/6 0 1/3 ] ; [1/6 1/3 0 ] ] 
+global _posterior_ = [ [0 0 0] ; [1/3 0 2/3 ] ; [0 0 0 ] ] 
+
+
+function prior(r,s) return _prior_[r,s] end
+function prior() return _prior_ end
+function posterior(r,s) return _posterior_[r,s] end
+function posterior() return _posterior_ end
+
+function Dkl(p,q)
+    res = 0.0
+    for i in 1:length(p)
+        if p[i]>0.0
+            res += p[i]*(log(q[i]/p[i]))
+    end end
+    return -res 
+end
+
+function alternative(i)
+    Dkl([ [0 0 0] ; [i/100 0 1-i/100 ] ; [0 0 0] ], _prior_)
+end
+
+maximum(alternative.(1:100))
+Dkl(_posterior_, _prior_)
+plot(alternative.(1:100))
