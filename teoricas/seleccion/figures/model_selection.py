86c02318ee4f2d35521060e19f46edba9b2aa070
Gustavo Landfried <gustavolandfried@gmail.com>
2021-08-30 20:04:47 -0300

    Teorica: selección de modelo

--- teoricas/seleccion/figures/model_selection.py
+++ teoricas/seleccion/figures/model_selection.py
@@ -72,7 +72,7 @@ def polynomial_basis_function(x, degree=1):
 N = 20
 
 beta = (1/0.2)**2
-alpha = (5e-7) # Bishop usa alpha = 5e-3
+alpha = (10e-6) # Bishop usa alpha = 5e-3
 
 # Data 
 X =np.random.rand(N,1)-0.5
@@ -83,12 +83,13 @@ X_grilla = np.linspace(0, 1, 100).reshape(-1, 1)-0.5
 y_grilla = np.linspace(-1.4, 1.4, 100).reshape(-1, 1)
 y_true = sinus_model(X_grilla , 0)
 
+
 plt.plot(X_grilla, y_true, '--', color="black")
 plt.plot(X,t,'.', color='black')
+plt.ylim(-1.5,1.5)
 plt.savefig("pdf/model_selection_true_and_sample.pdf",bbox_inches='tight')
 plt.savefig('png/model_selection_true_and_sample.png', bbox_inches='tight', transparent=True)
 plt.close()    
-
 
 prior_predictive_online = np.zeros((10,1))
 prior_maxAposteriori_online = np.zeros((10,1))
@@ -98,37 +99,40 @@ log_evidence_joint = np.zeros((10,1))
 w_map = []
 maxAposteriori = []
 maxApriori = []
-for d in range(10):#d=0
-    for i in range(N) :#i=2
-        X_priori = X[:i]
-        t_priori = t[:i]
-        x_posterior = X[i]
-        t_posteriori = t[i]
-        # Design matrix of training observations
-        Phi_priori =  polynomial_basis_function(X_priori, np.array(range(d+1)) )
-        Phi_posteriori = polynomial_basis_function(x_posterior , np.array(range(d+1)))
-        Phi_posteriori = Phi_posteriori.reshape((1,d+1))
-        
-        prior_predictive_online[d,0] += np.log(predictive(t_posteriori, Phi_posteriori, beta, alpha, t_priori, Phi_priori ))
-        
-        ## Otros indicadores.
-        w_map_prior = posterior(alpha, beta, t_priori, Phi_priori)[0]
-        prior_maxAposteriori_online[d,0] += np.log(likelihood(w_map_prior, t_posteriori, Phi_posteriori , beta))
-        mean_square_error_MAP[d,0] += (1/N) * ((t_posteriori - Phi_posteriori.dot(w_map_prior))**2)
-        
-                
-    Phi =  polynomial_basis_function(X, np.array(range(d+1)) )
-    prior_predictive_joint[d,0] = np.log(predictive(t, Phi, beta, alpha ))
-    log_evidence_joint[d,0] = log_evidence(t, Phi, beta, alpha)
-    w_map.append(posterior(alpha, beta, t, Phi)[0]) 
-    maxAposteriori.append(likelihood(w_map[d], t, Phi, beta))
-    # Joint max_a_priori
-    "Joint max a priori es igual para todas las complejidades (Es independiente de las cantidad de par\'ametros)"
-    Phi_priori =  polynomial_basis_function(X[:0], np.array(range(d+1)) )
-    t_priori =  t[:0]
-    w_maxAprior = posterior(alpha, beta, t_priori, Phi_priori )[0]
-    maxApriori.append(likelihood(w_maxAprior , t, Phi, beta)[0])
 
+def fit(alpha):
+    for d in range(10):#d=0
+        for i in range(N) :#i=2
+            X_priori = X[:i]
+            t_priori = t[:i]
+            x_posterior = X[i]
+            t_posteriori = t[i]
+            # Design matrix of training observations
+            Phi_priori =  polynomial_basis_function(X_priori, np.array(range(d+1)) )
+            Phi_posteriori = polynomial_basis_function(x_posterior , np.array(range(d+1)))
+            Phi_posteriori = Phi_posteriori.reshape((1,d+1))
+            
+            prior_predictive_online[d,0] += np.log(predictive(t_posteriori, Phi_posteriori, beta, alpha, t_priori, Phi_priori ))
+            
+            ## Otros indicadores.
+            w_map_prior = posterior(alpha, beta, t_priori, Phi_priori)[0]
+            prior_maxAposteriori_online[d,0] += np.log(likelihood(w_map_prior, t_posteriori, Phi_posteriori , beta))
+            mean_square_error_MAP[d,0] += (1/N) * ((t_posteriori - Phi_posteriori.dot(w_map_prior))**2)
+            
+                    
+        Phi =  polynomial_basis_function(X, np.array(range(d+1)) )
+        prior_predictive_joint[d,0] = np.log(predictive(t, Phi, beta, alpha ))
+        log_evidence_joint[d,0] = log_evidence(t, Phi, beta, alpha)
+        w_map.append(posterior(alpha, beta, t, Phi)[0]) 
+        maxAposteriori.append(likelihood(w_map[d], t, Phi, beta))
+        # Joint max_a_priori
+        "Joint max a priori es igual para todas las complejidades (Es independiente de las cantidad de par\'ametros)"
+        Phi_priori =  polynomial_basis_function(X[:0], np.array(range(d+1)) )
+        t_priori =  t[:0]
+        w_maxAprior = posterior(alpha, beta, t_priori, Phi_priori )[0]
+        maxApriori.append(likelihood(w_maxAprior , t, Phi, beta)[0])
+
+fit(alpha)
 plt.close()
 #plt.plot(prior_predictive_joint)
 plt.plot(np.exp(log_evidence_joint))
@@ -144,44 +148,99 @@ plt.savefig('png/model_selection_maxApriori_online.png', bbox_inches='tight',tra
 plt.close()
 
 plt.close()
-plt.plot(np.exp(prior_maxAposteriori_online/N))
-plt.savefig("pdf/model_selection_maxApriori_online_avg_prediction.pdf",bbox_inches='tight')
-plt.savefig('png/model_selection_maxApriori_online_avg_prediction.png', bbox_inches='tight',transparent=True)
-plt.close()
-
-
-plt.close()
-#plt.plot(prior_predictive_joint)
-plt.plot(np.exp(log_evidence_joint/N))
-plt.plot(np.exp(prior_predictive_online/N))
-plt.savefig("pdf/model_selection_evidence_avg_prediction.pdf",bbox_inches='tight')
-plt.savefig("png/model_selection_evidence_avg_prediction.png",bbox_inches='tight',transparent=True)
-plt.close()        
-
-plt.close()
-plt.plot(np.log(maxAposteriori))
-plt.savefig("pdf/model_selection_maxLikelihood_log.pdf",bbox_inches='tight')
-plt.savefig("png/model_selection_maxLikelihood_log.png",bbox_inches='tight',transparent=True)
-plt.close()        
-
-plt.close()
 plt.plot(maxAposteriori)
 plt.savefig("pdf/model_selection_maxLikelihood.pdf",bbox_inches='tight')
 plt.savefig("png/model_selection_maxLikelihood.png",bbox_inches='tight',transparent=True)
 plt.close()        
 
-for d in range(1,10):
+
+prior_predictive_online = np.zeros((10,1))
+prior_maxAposteriori_online = np.zeros((10,1))
+mean_square_error_MAP = np.zeros((10,1))
+prior_predictive_joint = np.zeros((10,1)) 
+log_evidence_joint = np.zeros((10,1)) 
+w_map = []
+maxAposteriori = []
+maxApriori = []
+
+fit(10**(-6))
+
+for d in range(0,10):#d=3
     Phi_grilla = polynomial_basis_function(X_grilla, np.array(range(d+1)) )
     y_map = Phi_grilla.dot(w_map[d])
     plt.plot(X_grilla,y_map  )
-    #plt.ylim(-1.1,1.1)
+
+
+plt.ylim(-1.5,1.5)
 Phi_grilla = polynomial_basis_function(X_grilla, np.array(range(3+1)) )
 y_map = Phi_grilla.dot(w_map[3])
 plt.plot(X_grilla,y_map, color="black"  )
+plt.plot(X_grilla, y_true, '--', color="black")
 plt.plot(X,t,'.', color='black')
-plt.savefig("pdf/model_selection_MAP.pdf",bbox_inches='tight')
-plt.savefig("png/model_selection_MAP.png",bbox_inches='tight',transparent=True)
+plt.savefig("pdf/model_selection_MAP_non-informative.pdf",bbox_inches='tight')
+plt.savefig("png/model_selection_MAP_non-informative.png",bbox_inches='tight',transparent=True)
 plt.close()    
+
+
+prior_predictive_online = np.zeros((10,1))
+prior_maxAposteriori_online = np.zeros((10,1))
+mean_square_error_MAP = np.zeros((10,1))
+prior_predictive_joint = np.zeros((10,1)) 
+log_evidence_joint = np.zeros((10,1)) 
+w_map = []
+maxAposteriori = []
+maxApriori = []
+
+fit(10**(-1))
+
+for d in range(0,10):#d=3
+    Phi_grilla = polynomial_basis_function(X_grilla, np.array(range(d+1)) )
+    y_map = Phi_grilla.dot(w_map[d])
+    plt.plot(X_grilla,y_map  )
+
+
+plt.ylim(-1.5,1.5)
+Phi_grilla = polynomial_basis_function(X_grilla, np.array(range(3+1)) )
+y_map = Phi_grilla.dot(w_map[3])
+plt.plot(X_grilla,y_map, color="black"  )
+plt.plot(X_grilla, y_true, '--', color="black")
+plt.plot(X,t,'.', color='black')
+plt.savefig("pdf/model_selection_MAP_informative.pdf",bbox_inches='tight')
+plt.savefig("png/model_selection_MAP_informative.png",bbox_inches='tight',transparent=True)
+plt.close()    
+
+
+
+###
+# El prior es regularizador, pero en el sentido contrario: un prior poco informativo favorece a los modelos de menor grado.
+
+
+prior_predictive_online = np.zeros((10,1))
+prior_maxAposteriori_online = np.zeros((10,1))
+mean_square_error_MAP = np.zeros((10,1))
+prior_predictive_joint = np.zeros((10,1)) 
+log_evidence_joint = np.zeros((10,1)) 
+w_map = []
+maxAposteriori = []
+maxApriori = []
+
+model_alpha = np.zeros((10,7))
+for i in range(7):
+    log_evidence_joint = np.zeros((10,1)) 
+    print(i)
+    fit(10**(-i))
+    model_alpha[:,i] = (np.exp(log_evidence_joint)/sum(np.exp(log_evidence_joint))).reshape((10,))
+    
+for i in range(10):
+    plt.plot(model_alpha[i,:], label=str(i) )
+
+
+plt.legend(loc="upper left")
+plt.savefig("pdf/regularizador.pdf",bbox_inches='tight')
+plt.savefig("png/regularizador.png",bbox_inches='tight',transparent=True)
+plt.close()    
+
+
 
 """
 i=16
