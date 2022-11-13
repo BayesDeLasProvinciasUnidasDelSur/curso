cf94d68ab67982802c26804183ba921c98038d28
Gustavo Landfried <gustavolandfried@gmail.com>
2022-11-13 18:04:19 +0100

    Principios. Código C#. Varios ejemplos

--- unidades/1-principios/code/ejemplos/Ejemplos.cs
+++ unidades/1-principios/code/ejemplos/Ejemplos.cs
@@ -40,7 +40,8 @@ public class Simples{
        Console.WriteLine("  P(Causa|Efecto = 1) = "+engine.Infer(causa));
      }
   }
-  public void seleccion_observacional_de_modelo_causal(){
+
+  public void seleccion_modelo_causal_mediante_observaciones(){
     Console.WriteLine("Selección observacional de modelo causal");
     Range N = new Range(5);
     // El espacio de modelos
@@ -68,10 +69,125 @@ public class Simples{
 
     bool[] dataA = {true, true, false, true, true};
     bool[] dataB = {true, true, false, true, false};
-    A.ObservedValue = dataB;
+    // ToDo: Porbar Variable<bool>[] para definir observable solo a algunas de las variables.
+    A.ObservedValue = dataA;
     B.ObservedValue = dataB;
     Bernoulli AcausesBdist = engine.Infer<Bernoulli>(AcausesB); // Infer posterior
     Console.WriteLine("  P(ModeloAB|Datos) = "+AcausesBdist);
+  }
+
+  public void seleccion_de_modelo_causal_mediante_intervenciones(){
+    Range N = new Range(8);
+    // El espacio de modelos
+    var AcausesB = Variable.Bernoulli(0.5);
+
+    // Las variables que comparten ambos modelos
+    var A = Variable.Array<bool>(N).Named("A");
+    var B = Variable.Array<bool>(N).Named("B");
+    var doB = Variable.Array<bool>(N).Named("doB");
+    using (Variable.If(AcausesB)){
+      // ModeloAB
+      using (Variable.ForEach(N)){
+        using (Variable.If(doB[N])){
+          A[N] = Variable.Bernoulli(0.5);
+          B[N] = Variable.Bernoulli(0.5);
+        }
+        using (Variable.IfNot(doB[N])){
+          A[N] = Variable.Bernoulli(0.5);
+          B[N] = A[N] != Variable.Bernoulli(0.05);
+        }
+      }
+    }
+    using (Variable.IfNot(AcausesB)){
+      // ModeloBA
+      using (Variable.ForEach(N)){
+        using (Variable.If(doB[N])){
+          B[N] = Variable.Bernoulli(0.5);
+          A[N] = B[N] != Variable.Bernoulli(0.05);
+        }
+        using (Variable.IfNot(doB[N])){
+          B[N] = Variable.Bernoulli(0.5);
+          A[N] = Variable.Bernoulli(0.5);
+        }
+      }
+    }
+    // Create an inference engine
+    var engine = new InferenceEngine();
+
+    bool[] boBdata =   {true, true, true, true, false, false, false, false};
+    bool[] dataA = {true, true, false, true, true, false, false, false};
+    bool[] dataB = {true, false, true, false, true, false, false, true};
+    A.ObservedValue = dataA;
+    B.ObservedValue = dataB;
+    doB.ObservedValue = boBdata;
+    Bernoulli AcausesBdist = engine.Infer<Bernoulli>(AcausesB); // Infer posterior
+    Console.WriteLine("  P(ModeloAB|Datos, doB) = "+AcausesBdist);
+  }
+  public void probabilidad_de_una_moneda(bool[] data){
+    Range i = new Range(data.Length);
+    Variable<double> probabilidad = Variable.Beta(1, 1);
+    var monedas =  Variable.Array<bool>(i);
+    monedas[i] = Variable.Bernoulli(probabilidad).ForEach(i);
+    monedas.ObservedValue = data;
+    var engine = new InferenceEngine();
+    Console.WriteLine("  P(probabilidad|Datos) = "+engine.Infer(probabilidad));
+  }
+  public void probabilidad_de_un_dado(int[] data){
+    var engine = new InferenceEngine();
+    Range i = new Range(data.Length);
+    var probabilidad = Variable.DirichletUniform(6);
+    Console.WriteLine("  P(probabilidad) = "+engine.Infer(probabilidad));
+    var tiradas =  Variable.Array<int>(i);
+    tiradas[i] = Variable.Discrete(probabilidad).ForEach(i);
+    tiradas.ObservedValue = data;
+    Console.WriteLine("  P(probabilidad|Datos) = "+engine.Infer(probabilidad));
+    var lados = Variable.Discrete(probabilidad);
+    Console.WriteLine("  P(lados|probabilidad) = "+engine.Infer(lados));
+  }
+  public void experimento_medico(bool[] control, bool[] tratamiento){
+    InferenceEngine engine = new InferenceEngine();
+    engine.ModelName = "Experimento médico";
+    engine.SaveFactorGraphToFolder = "graphs";
+    //engine.ShowFactorGraph = true;
+
+    Range i_c = new Range(control.Length);
+    Range i_t = new Range(tratamiento.Length);
+    var grupo_control =  Variable.Array<bool>(i_c).Named("Control");
+    var grupo_tratado =  Variable.Array<bool>(i_t).Named("Tratado");
+
+    Variable<bool> esEfectivo = Variable.Bernoulli(0.5).Named("Efecto");
+
+    using (Variable.If(esEfectivo)){
+      Variable<double> probC = Variable.Beta(1, 1).Named("ProbC");
+      grupo_control[i_c] = Variable.Bernoulli(probC).ForEach(i_c);
+      Variable<double> probT = Variable.Beta(1, 1).Named("ProbT");
+      grupo_tratado[i_t] = Variable.Bernoulli(probT).ForEach(i_t);
+    }
+    using (Variable.IfNot(esEfectivo)){
+      Variable<double> probAll = Variable.Beta(1, 1).Named("ProbAll");
+      grupo_control[i_c] = Variable.Bernoulli(probAll).ForEach(i_c);
+      grupo_tratado[i_t] = Variable.Bernoulli(probAll).ForEach(i_t);
+    }
+
+    grupo_control.ObservedValue = control;
+    grupo_tratado.ObservedValue = tratamiento;
+
+    Console.WriteLine("P(esEfectivo|Data) = "+ engine.Infer(esEfectivo));
+  }
+  public void sorpresa_de_un_dado(int[] data){
+    Variable<bool> evidence = Variable.Bernoulli(0.5).Named("evidence");
+
+    IfBlock block = Variable.If(evidence);
+    Range i = new Range(data.Length);
+    var probabilidad = Variable.DirichletUniform(6);
+    var tiradas =  Variable.Array<int>(i);
+    tiradas[i] = Variable.Discrete(probabilidad).ForEach(i);
+    tiradas.ObservedValue = data;
+    block.CloseBlock();
+
+    var engine = new InferenceEngine();
+    double logEvidence = engine.Infer<Bernoulli>(evidence).LogOdds;
+    Console.WriteLine("  P(Datos)"+Math.Exp(logEvidence/data.Length));
   }
 }
 
@@ -82,9 +198,15 @@ public class Simples{
 class Program{
   static void Main(string[] args){
     var simples = new Simples();
-    simples.modelo_causal_con_ruido(true);
-    simples.modelo_causal_con_ruido(false);
-    simples.seleccion_observacional_de_modelo_causal();
+    //simples.logica_con_incertidumbre();
+    //simples.modelo_causal_con_ruido(true);
+    //simples.modelo_causal_con_ruido(false);
+    //simples.seleccion_modelo_causal_mediante_observaciones();
+    //simples.seleccion_de_modelo_causal_mediante_intervenciones();
+    //simples.probabilidad_de_una_moneda(new bool[] {true, false,true,true,true,false});
+    //simples.probabilidad_de_un_dado(new int[] {0,1,1,1,4,3,2,5});
+    //simples.experimento_medico(new bool[] {false,true,false,false,false}, new bool[] {true,true,false,true,true});
+    simples.sorpresa_de_un_dado(new int[] {0,0,0,0,0,0,1,0,1});
     Console.WriteLine("Chau mundo!");
   }
 }
--- unidades/1-principios/code/montyHall/MontyHall.cs
+++ unidades/1-principios/code/montyHall/MontyHall.cs
@@ -1,13 +1,56 @@
-﻿using Microsoft.ML.Probabilistic.Algorithms;
+﻿// https://github.com/dotnet/infer/blob/57c888024c690ad2fdfd2f70214848485451c0e3/src/Examples/MontyHall/Window1.xaml.cs#L35
+using Microsoft.ML.Probabilistic.Algorithms;
 using Microsoft.ML.Probabilistic.Distributions;
 using Microsoft.ML.Probabilistic.Models;
 using Range = Microsoft.ML.Probabilistic.Models.Range;
 
-// Modelo
-var regalo = Variable.Discrete([1.0/3.0,1.0/3.0,1.0/3.0]);
-var eleccion = Variable.Discrete([1.0/3.0,1.0/3.0,1.0/3.0]);
+namespace MontyHall{
+public class Funciones{
+  public void ejemplo1(int e, int d){
+    // Modelo
 
-// El sistema de inferencia
-InferenceEngine engine = new InferenceEngine();
+    double[] prior = {1.0/3.0,1.0/3.0,1.0/3.0};
+    var regalo = Variable.Discrete(prior);
+    var eleccion = Variable.DiscreteUniform(3).Named("eleccion");
+    var pista =  Variable.New<int>().Named("pista");
+    // Variable<bool> pistaIsObserved;
+    // pistaIsObserved = Variable.Observed<bool>(false);
+    for (int a = 0; a < 3; a++){
+      for (int b = 0; b < 3; b++){
+        double[] probs = { 1, 1, 1 };
+        for (int ps = 0; ps < 3; ps++){
+          if (ps == a || ps == b){
+            probs[ps] = 0;
+          }
+        }
+        using (Variable.Case(eleccion, a)){
+          using (Variable.Case(regalo, b)){
+            Console.WriteLine(probs[0]+","+probs[1]+","+probs[2]);
+            pista.SetTo(Variable.Discrete(probs));
+          }
+        }
+      }
+    }
 
-Console.WriteLine("End Monty Hall!");
+    InferenceEngine engine = new InferenceEngine();
+
+    Console.WriteLine("  Posterior del pista: "+engine.Infer(pista));
+    eleccion.ObservedValue = e;
+    Console.WriteLine("  Posterior del pista: "+engine.Infer(pista));
+    pista.ObservedValue = d;
+    Console.WriteLine("  Posterior del regalo: "+engine.Infer(regalo));
+
+    Console.WriteLine("End Monty Hall!");
+  }
+}
+
+
+class Program{
+  static void Main(string[] args){
+    var funciones = new Funciones();
+    funciones.ejemplo1(0,1);
+    Console.WriteLine("Chau mundo!");
+  }
+}
+
+}
