84b6ec7af30ad5a19448e7dfea064711be42c3e9
Gustavo Landfried <gustavolandfried@gmail.com>
2022-11-20 20:44:10 +0100

    Code. C#. Modulos. CSV reader. Mixture Model

--- unidades/1-principios/code/ejemplos/Ejemplos.cs
+++ unidades/1-principios/code/ejemplos/Ejemplos.cs
@@ -189,29 +189,9 @@ public class Simples{
     double logEvidence = engine.Infer<Bernoulli>(evidence).LogOdds;
     Console.WriteLine("  P(Datos)"+Math.Exp(logEvidence/data.Length));
   }
-  public void probabilidad_de_un_dado_en_el_tiempo(int[] data){
+  //public void probabilidad_de_un_dado_en_el_tiempo(int[] data){
     // Vamos a hacer un modelo que estime una probabilidad del dado que cambia en el tiempo.
-  }
-}
-
-
-
-
-
-class Program{
-  static void Main(string[] args){
-    var simples = new Simples();
-    //simples.logica_con_incertidumbre();
-    //simples.modelo_causal_con_ruido(true);
-    //simples.modelo_causal_con_ruido(false);
-    //simples.seleccion_modelo_causal_mediante_observaciones();
-    //simples.seleccion_de_modelo_causal_mediante_intervenciones();
-    //simples.probabilidad_de_una_moneda(new bool[] {true, false,true,true,true,false});
-    simples.probabilidad_de_un_dado(new int[] {0,1,1,1,4,3,2,5});
-    //simples.experimento_medico(new bool[] {false,true,false,false,false}, new bool[] {true,true,false,true,true});
-    simples.sorpresa_de_un_dado(new int[] {0,0,0,0,0,0,1,0,1});
-    Console.WriteLine("Chau mundo!");
-  }
+  //}
 }
 
 
--- unidades/1-principios/code/mixtureModel/mixtureModel.cs
+++ unidades/1-principios/code/mixtureModel/mixtureModel.cs
@@ -2,8 +2,10 @@
 using Microsoft.ML.Probabilistic.Algorithms;
 using Microsoft.ML.Probabilistic.Distributions;
 using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Math;
 using Range = Microsoft.ML.Probabilistic.Models.Range;
 namespace MixtureModels{
+// https://dotnet.github.io/infer/userguide/Mixture%20of%20Gaussians%20tutorial.html
 // https://towardsdatascience.com/bayesian-gaussian-mixture-models-without-the-math-using-infer-net-7767bb7494a0#:~:text=Bayesian Gaussian mixture models constitutes,outlier detection, or generative classifiers.
 public class Models{
   public void inferir_media_de_una_gaussian(double[] data){
@@ -27,6 +29,8 @@ public class Models{
     Console.WriteLine("");
   }
   public void infer_media_y_precision_de_una_gaussiana(double[] data){
+    Console.WriteLine("infer_media_y_precision_de_una_gaussiana");
+
     Range numDataPoints = new Range(data.Length);
     Variable<double> mean = Variable.GaussianFromMeanAndPrecision(0, 0.01).Named("Mean");
     Variable<double> precision = Variable.GammaFromShapeAndRate(2, 1);
@@ -47,27 +51,92 @@ public class Models{
 
     Console.WriteLine("");
   }
-  public void infer_media_y_precision_de_k_gaussiana(){
+  public void infer_media_y_precision_de_k_gaussiana(double[] dataK, int K){
+    Console.WriteLine("infer_media_y_precision_de_k_gaussiana");
+
+    Range numDataPoints = new Range(dataK.Length);
+    Range k = new Range(K);
+
+    VariableArray<double> means = Variable.Array<double>(k).Named("Means");
+    for (int i = 0; i < K; i++)
+      means[i] = Variable.GaussianFromMeanAndPrecision(i, 0.1);
+
+    var engine = new InferenceEngine();
+    //engine.Algorithm = new VariationalMessagePassing();
+    Gaussian[] postMean = engine.Infer<Gaussian[]>(means);
+    for (int i = 0; i < K; i++){
+        Console.WriteLine("P(mean{0}|Data) = ", postMean[i]);
+    }
+
+
+    VariableArray<double> precisions = Variable.Array<double>(k).Named("Precisions");
+    precisions[k] = Variable.GammaFromShapeAndRate(2, 1).ForEach(k);
+
+    VariableArray<double> x = Variable.Array<double>(numDataPoints).Named("x");
+
+    VariableArray<int> z = Variable.Array<int>(numDataPoints).Named("z");
+
+    using (Variable.ForEach(numDataPoints)){
+      z[numDataPoints] = Variable.Discrete(k, new double[] { 1.0/3.0, 1.0/3.0, 1.0/3.0 });
+
+      using (Variable.Switch(z[numDataPoints])){
+        x[numDataPoints] = Variable.GaussianFromMeanAndPrecision( means[z[numDataPoints]], precisions[z[numDataPoints]]);
+      }
+    }
+
+    x.ObservedValue = dataK;
+
+
+    postMean = engine.Infer<Gaussian[]>(means);
+    Gamma[] postPrecision = engine.Infer<Gamma[]>(precisions);
+    Discrete[] postZ = engine.Infer<Discrete[]>(z);
+
+    for (int i = 0; i < K; i++){
+        Console.WriteLine("P(mean{1}|Data) = {0}", postMean[i],i);
+        Console.WriteLine("P(precision{1}|Data = {0}", postPrecision[i],i);
+    }
+    for (int i = 0; i < 5; i++)
+        Console.WriteLine("x = "+dataK[i]+ ": p(z) = "+postZ[i]);
+
     Console.WriteLine("");
   }
   public void infer_media_precision_y_pesos_de_k_gaussiana(){
     Console.WriteLine("");
   }
   public void seleccion_de_la_complejidad_k_del_modelo(){
+    Console.WriteLine("seleccion_de_la_complejidad_k_del_modelo");
+
     Console.WriteLine("");
   }
 }
 
-class Program{
-  static void Main(string[] args){
-    double[] data1 = {4.97927135527903, 4.59200148907175, 5.186440451964069, 4.799267690885445, 4.44323651636977, 5.516809467200307, 4.878760996592614, 4.733303790317494, 4.819648433009107, 4.75018621913176, 5.047937996897632, 4.394096465840763, 4.877933263872199, 4.717038541890403, 4.932324049872676, 5.137812765262641};
+// class Program{
+//   static void Main(string[] args){
+//     double[] data1 = {4.97927135527903, 4.59200148907175, 5.186440451964069, 4.799267690885445, 4.44323651636977, 5.516809467200307, 4.878760996592614, 4.733303790317494, 4.819648433009107, 4.75018621913176, 5.047937996897632, 4.394096465840763, 4.877933263872199, 4.717038541890403, 4.932324049872676, 5.137812765262641};
+//
+//     double[] dataK = {4.97927135527903, 4.59200148907175, 5.186440451964069, 4.799267690885445, 4.44323651636977, 5.516809467200307, 4.878760996592614, 4.733303790317494, 4.819648433009107, 4.75018621913176, 5.047937996897632, 4.394096465840763, 4.877933263872199, 4.717038541890403, 4.932324049872676, 5.137812765262641};
+//
+//     var modelos = new Models();
+//     modelos.inferir_media_de_una_gaussian(data1);
+//     modelos.infer_media_y_precision_de_una_gaussiana(data1);
+//     Console.WriteLine("Chau mundo!");
+//   }
+// }
+//
 
-    double[] dataK = {4.97927135527903, 4.59200148907175, 5.186440451964069, 4.799267690885445, 4.44323651636977, 5.516809467200307, 4.878760996592614, 4.733303790317494, 4.819648433009107, 4.75018621913176, 5.047937996897632, 4.394096465840763, 4.877933263872199, 4.717038541890403, 4.932324049872676, 5.137812765262641};
-
-    var modelos = new Models();
-    modelos.inferir_media_de_una_gaussian(data1);
-    modelos.infer_media_y_precision_de_una_gaussiana(data1);
-    Console.WriteLine("Chau mundo!");
+public class Funciones{
+  public List<double> read_csv(string path){
+    List<double> data2 = new List<double>();
+    using(var reader = new StreamReader(@path)){
+      //List<string> listA = new List<string>();
+      while (!reader.EndOfStream){
+        var line = reader.ReadLine();
+        var values = line.Split('\n');
+        data2.Add(Convert.ToDouble(values[0]));
+        //listA.Add(values);
+      }
+    }
+    return data2;
   }
 }
 
