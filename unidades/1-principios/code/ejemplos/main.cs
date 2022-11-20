5398d27ec7196b6ae5558adbfc8652cb9bb0cebc
Gustavo Landfried <gustavolandfried@gmail.com>
2022-11-20 20:44:24 +0100

    Code. C#. Modulos. CSV reader. Mixture Model

---
+++ unidades/1-principios/code/ejemplos/main.cs
@@ -0,0 +1,20 @@
+﻿using Ejemplos;
+using Microsoft.ML.Probabilistic.Algorithms;
+using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Models;
+using Range = Microsoft.ML.Probabilistic.Models.Range;
+
+
+var simples = new Simples();
+
+//simples.logica_con_incertidumbre();
+//simples.modelo_causal_con_ruido(true);
+//simples.modelo_causal_con_ruido(false);
+//simples.seleccion_modelo_causal_mediante_observaciones();
+//simples.seleccion_de_modelo_causal_mediante_intervenciones();
+//simples.probabilidad_de_una_moneda(new bool[] {true, false,true,true,true,false});
+simples.probabilidad_de_un_dado(new int[] {0,1,1,1,4,3,2,5});
+//simples.experimento_medico(new bool[] {false,true,false,false,false}, new bool[] {true,true,false,true,true});
+simples.sorpresa_de_un_dado(new int[] {0,0,0,0,0,0,1,0,1});
+Console.WriteLine("Chau mundo!");
+
---
+++ unidades/1-principios/code/mixtureModel/main.cs
@@ -0,0 +1,14 @@
+﻿using MixtureModels;
+using System.IO;
+
+var fun = new MixtureModels.Funciones();
+
+double[] data1 = fun.read_csv("data/data1.csv").ToArray();
+double[] dataK = fun.read_csv("data/dataK.csv").ToArray();
+
+var modelos = new MixtureModels.Models();
+modelos.inferir_media_de_una_gaussian(data1);
+modelos.infer_media_y_precision_de_una_gaussiana(data1);
+modelos.infer_media_y_precision_de_k_gaussiana(dataK, 3);
+Console.WriteLine("Chau mundo!");
+
